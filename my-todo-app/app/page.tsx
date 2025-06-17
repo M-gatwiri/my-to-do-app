fa9e63acf846fa430fc40e32f38988a99f42cd4f
M-gatwiri <mgatwiri806@gmail.com>
2025-06-17 22:24:37 +0300

    Made changes in the todo pages

--- my-todo-app/app/page.tsx
+++ my-todo-app/app/page.tsx
@@ -1,12 +1,5 @@
-'use client';
-
 import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    redirect('/todos');
-  }, []);
-
-  return null;
+  redirect('/todos');
 }
